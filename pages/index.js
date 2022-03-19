c108879ed0de393cc4c0e46943d585bf5b3a61e5
MSAZ89 <michaelsimonaz@gmail.com>
2022-03-19 14:16:02 -0700

    Header.js

--- pages/index.js
+++ pages/index.js
@@ -1,16 +1,13 @@
 import { useState } from "react"
 
+import Header from "./comps/header";
+
 export default function Home() {
   const [data, updateData] = useState("Data");
 
   return (
     <div className="container mx-auto py-10">
-      <button className="bg-red-400 px-4 py-1" onClick={ () => updateData(document.getElementById("data").value)}>Update Data</button>
-      <br/><br/>
-      <input className="bg-red-100" id="data" type="text" />
-      <br/><br/>
-      <p>{data}</p>
-
+      <Header/>
     </div>
   )
 }

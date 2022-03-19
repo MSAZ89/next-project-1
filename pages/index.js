1a839bd633b57f9174f28f141117e90aee5cb800
MSAZ89 <michaelsimonaz@gmail.com>
2022-03-19 14:48:16 -0700

    Styling + useState Implementation

--- pages/index.js
+++ pages/index.js
@@ -3,10 +3,10 @@ import { useState } from "react"
 import Header from "./comps/header";
 
 export default function Home() {
-  const [data, updateData] = useState("Data");
+  const [data, updateData] = useState("");
 
-  const h1element = <h1 className="text-6xl">H1 Tag</h1>
-  const description = <p>Next.js + Tailwind.css test</p>
+  const h1element = <h1 className="text-6xl">Tailwind CSS + Next.js</h1>
+  const description = <p>Something something clever..</p>
 
   return (
     <div className="container mx-auto py-5">
@@ -14,9 +14,19 @@ export default function Home() {
       <div className="flex items-stretch place-content-center py-5">
         {h1element}
       </div>
+
       <div className="flex items-stretch place-content-center">
         {description}
       </div>
+
+      <div className="flex items-stretch place-content-center py-5">
+        <input className="bg-gray-100" id="input" onChange={ () => updateData(document.getElementById("input").value)} type="text"/>
+      </div>
+
+      <div className="flex items-stretch place-content-center">
+        <p>{data}</p>
+      </div>
+
     </div>
   )
 }
